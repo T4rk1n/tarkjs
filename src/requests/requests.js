dbeff8a73e07232a00ec34fd1e2b72c2f13a9ade
T4rk1n <t4rk@outlook.com>
2017-07-25 20:39:01 -0400

    Refactored requests to CancelablePromise.

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -7,40 +7,42 @@ import { objItems } from '../extensions/obj-extensions'
 const jsonPattern = /json/i
 
 /**
- * @typedef {Object} FetchOptions
- * @property {boolean} rejectNotOk to reject the promise when the response is not successful (statusCode >= 400).
- */
-
-/**
- * @type {FetchOptions}
- */
-const defaultFetchOptions = {
-    rejectNotOk: true
-}
-
-/**
  * Fetch promise wrap
  *
  * If you don't care about the response headers and meta, use this to stay dry.
  * Intended to be used with the PromiseStore so it have a valid resolve value when fulfilled.
  *
  * If the response is json, it will automatically parse the value to an object.
+ * Cancellation: unsupported by the fetch api, rejection on return. Use xhr if there's a need to cancel.
  * @param {!string} url the address to fetch
  * @param {Object} [init={}] the fetch init object.
- * @param {FetchOptions} [options={rejectNotOk: true}]
+ * @return {CancelablePromise} Not really cancelable
  */
-export const fetchRequest = (url, init={}, options=defaultFetchOptions) => new Promise((resolve, reject) => {
-    const { rejectNotOk } = {...defaultFetchOptions, ...options}
-    fetch(url, init).then((rep) => {
-        if (rep.ok) {
-            if (jsonPattern.test(rep.headers['Content-Type'])) rep.json().then(value => resolve(value))
-            else rep.text().then(value => resolve(value))
-        } else {
-            if (rejectNotOk) reject(`FETCH ${url} FAILED - STATUS: ${rep.statusCode} MESSAGE: ${rep.statusText}`)
-        }
-    }).catch(err => reject(err))
-})
+export const fetchRequest = (url, init={}) => {
+    let canceled
+    const promise = new Promise((resolve, reject) => {
+        fetch(url, init).then((rep) => {
+            if (canceled) reject({
+                error: 'canceled',
+                message: `Fetch was canceled. status: ${rep.statusCode}`,
+                response: rep
+            })
+            else if (rep.ok) {
+                if (jsonPattern.test(rep.headers['Content-Type'])) rep.json().then(value => resolve(value))
+                else rep.text().then(value => resolve(value))
+            }
+            else {
+                reject({
+                    error: 'fetch_fail',
+                    message: `Fetch ${url} failed, status: ${rep.statusCode}, message: ${rep.statusText}`,
+                    response: rep
+                })
+            }
+        }).catch(err => reject(err))
 
+    })
+    return {promise, cancel: () => canceled = true}
+}
 /**
  * @typedef {Object} XhrOptions
  * @property {string} [method='GET']
@@ -63,31 +65,43 @@ const defaultXhrOptions = {
  * Fetch can't do put request, so xhr still useful.
  *
  * Auto parse json responses.
+ * Cancellation: xhr.abort
  * @param {string} url
  * @param {XhrOptions} [options]
+ * @return {CancelablePromise}
  */
-export const xhrRequest = (url, options=defaultXhrOptions) => new Promise((resolve, reject) => {
-    const { method, headers, payload } = {...defaultXhrOptions, ...options}
-    const xhr = new XMLHttpRequest()
-    xhr.open(method, url)
-    objItems(headers).forEach(([k, v]) => xhr.setRequestHeader(k, v))
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                let responseValue = xhr.response
-                if (jsonPattern.test(xhr.getResponseHeader('Content-Type'))) {
-                    responseValue = JSON.parse(xhr.responseText)
+export const xhrRequest = (url, options=defaultXhrOptions) => {
+    let cancel = () => {}
+    const promise = new Promise((resolve, reject) => {
+        const {method, headers, payload} = {...defaultXhrOptions, ...options}
+        const xhr = new XMLHttpRequest()
+        xhr.open(method, url)
+        objItems(headers).forEach(([k, v]) => xhr.setRequestHeader(k, v))
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    let responseValue = xhr.response
+                    if (jsonPattern.test(xhr.getResponseHeader('Content-Type'))) {
+                        responseValue = JSON.parse(xhr.responseText)
+                    }
+                    resolve(responseValue)
+                } else {
+                    if (xhr.status === 0) reject({error: 'canceled', message: 'XHR was aborted.'})
+                    else reject({
+                        error: 'xhr_fail',
+                        message: `XHR ${url} FAILED - STATUS: ${xhr.status} MESSAGE: ${xhr.statusText}`,
+                        xhr
+                    })
                 }
-                resolve(responseValue)
-            } else {
-                reject({
-                    error: 'xhr_fail',
-                    message: `XHR ${url} FAILED - STATUS: ${xhr.status} MESSAGE: ${xhr.statusText}`,
-                    xhr
-                })
             }
         }
-    }
-    xhr.onerror = (err) => reject(err)
-    xhr.send(payload)
-})
+        xhr.onerror = (err) => reject(err)
+        xhr.send(payload)
+        cancel = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                xhr.abort()
+            }
+        }
+    })
+    return { promise, cancel }
+}
--- test/requests-spec.js
+++ test/requests-spec.js
@@ -5,28 +5,52 @@ import { fetchRequest, xhrRequest } from '../src/requests/requests'
 
 describe('Test requests wrappers', () => {
     it('Test fetchRequest', (done) => {
-        fetchRequest('base/test/hello.txt').then(data => {
+        const { promise } = fetchRequest('base/test/hello.txt')
+        promise.then(data => {
             expect(data).toBe('hello')
             done()
         })
     })
+
+    it('Test fetch failure', (done) => {
+        const { promise } = fetchRequest('noneexist.json')
+        promise.catch(err => {
+            expect(err.error).toBe('fetch_fail')
+            done()
+        })
+    })
+
     it('Test the xhrRequest text result', (done) => {
-        xhrRequest('base/test/hello.txt').then((data) => {
+        const { promise } = xhrRequest('base/test/hello.txt')
+        promise.then((data) => {
             expect(data).toBe('hello')
             done()
         })
     })
+
     it('Test the xhrRequest json result', (done) => {
-        xhrRequest('base/test/test.json').then((value) => {
+        const { promise } = xhrRequest('base/test/test.json')
+        promise.then((value) => {
             expect(value.Hello).toBe('test')
             done()
         })
     })
+
     it('Test the xhrRequest failure', (done) => {
-        xhrRequest('noneexist.json').catch(err => {
+        const { promise } = xhrRequest('noneexist.json')
+        promise.catch(err => {
             expect(err.error).toBe('xhr_fail')
             done()
         })
+    })
+
+    it('Test the xhrRequest cancelation', (done) => {
+        const { promise, cancel } = xhrRequest('base/test/test.json')
+        promise.catch(err => {
+            expect(err.error).toBe('canceled')
+            done()
+        })
+        cancel()
     })
 })
 
